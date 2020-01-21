bc5d2766811b88d2fa6e6cce1bd151bc68ace1dc
Alexandre Fernandes Junior <eu@alejr.com.br>
2020-01-21 15:12:34 -0300

    Apollo config and vuex high order function created

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import apolloProvider from './apollo'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider,
+  created () {
+    this.$store.dispatch('initUserFromLocalStorage')
+    this.$store.dispatch('initMeasurementsFromLocalStorage')
+  },
   render: h => h(App)
 }).$mount('#app')
